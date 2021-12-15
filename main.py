1f742f68a709cbfa5ca91b11316ad985ce773bf2
IvanBesedin <besedaivan999@gmail.com>
2021-12-15 14:12:24 +1000

    Initial commit

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+temps = []
+
 
 @app.get("/")
-async def root():
-    return "Hello World"
+def read_root():
+    return {"Hello": "World"}
+    return temps
+
+
+@app.get("/items/{item_id}")
+@@ -24,4 +25,5 @@ def webhook(item):
+@app.post("/webhook/uplink")
+def uplink(item: dict):
+    print(item)
+    temps.append(item["uplink_message"]["decoded_payload"])
+    return {"success": True}
