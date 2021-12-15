34cd27d2e4ee3f91e868488b8499a732032fcc30
IvanBesedin <besedaivan999@gmail.com>
2021-12-15 14:57:17 +1000

    Initial commit

--- main.py
+++ main.py
@@ -9,11 +9,6 @@ def read_root():
     return temps
 
 @app.post("/webhook")
-def webhook(item):
-    print(item)
-    return {"success": True}
-
-@app.post("/webhook/uplink")
 def uplink(item: dict):
     print(item)
     temps.append(item["uplink_message"]["decoded_payload"])
