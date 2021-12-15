a957cd71ec412aac5f04e4c87ac831b3fc6b979f
IvanBesedin <besedaivan999@gmail.com>
2021-12-15 14:51:38 +1000

    Initial commit

--- main.py
+++ main.py
@@ -9,14 +9,11 @@ def read_root():
     return {"Hello": "World"}
     return temps
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
 @app.post("/webhook")
 def webhook(item):
     print(item)
     return {"success": True}
+
 @app.post("/webhook/uplink")
 def uplink(item: dict):
     print(item)
